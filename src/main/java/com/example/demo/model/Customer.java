05c329aff201f3b52f012fd3d1e95c7fd06a1214
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2020-04-03 21:52:57 +0100

    Added customer model.

--- src/main/java/com/example/demo/model/Customer.java
+++ src/main/java/com/example/demo/model/Customer.java
@@ -1,5 +1,36 @@
 package com.example.demo.model;
 
 public class Customer {
+  private int id;
+  private String firstName;
+  private String lastName;
+  private String email;
 
+  /**
+   * @param email the email to set
+   */
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  /**
+   * @param id the id to set
+   */
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  /**
+   * @param firstName the firstName to set
+   */
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  /**
+   * @param lastName the lastName to set
+   */
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
 }
