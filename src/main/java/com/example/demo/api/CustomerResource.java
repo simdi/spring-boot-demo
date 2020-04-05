ad89ba31d6ff791c530146a1990092af02b4dfe3
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2020-04-05 20:23:44 +0100

    Done with customer service.

---
+++ src/main/java/com/example/demo/api/CustomerResource.java
@@ -0,0 +1,48 @@
+package com.example.demo.api;
+
+import java.util.List;
+
+import com.example.demo.models.Customer;
+import com.example.demo.services.CustomerService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/customers")
+public class CustomerResource {
+  @Autowired
+  private CustomerService customerService;
+
+  @GetMapping
+  public List<Customer> getCustomers() {
+    return customerService.getCustomers();
+  }
+
+  @GetMapping(value = "/{id}")
+  public Customer getCustomer(@PathVariable("id") int id) {
+    return customerService.getCustomer(id);
+  }
+
+  @PostMapping
+  public Customer addCustomer(@RequestBody Customer customer) {
+    return customerService.addCustomer(customer);
+  }
+
+  @PutMapping(value = "/{id}")
+  public Customer updatedCustomer(@PathVariable("id") int id, Customer customer) {
+    return customerService.updateCustomer(id, customer);
+  }
+
+  @DeleteMapping(value = "/{id}")
+  public void deleteCustomer(@PathVariable("id") int id) {
+    customerService.deleteCustomer(id);
+  }
+}
--- src/main/java/com/example/demo/services/CustomerService.java
+++ src/main/java/com/example/demo/services/CustomerService.java
@@ -3,8 +3,12 @@ package com.example.demo.services;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import org.springframework.stereotype.Component;
+
 import com.example.demo.models.Customer;
 
+
+@Component
 public class CustomerService {
   private int customerIdCount = 1;
   private List<Customer> customerList = new CopyOnWriteArrayList<>();
@@ -20,18 +24,20 @@ public class CustomerService {
     return customerList;
   }
 
-  public Customer getCustomer(int customerId) {
-    return customerList.stream().filter(c -> c.getId() == customerId).findFirst().get();
+  public Customer getCustomer(int id) {
+    return customerList.stream().filter(c -> c.getId() == id).findFirst().get();
   }
 
   public Customer updateCustomer(int id, Customer customer) {
-    customerList.stream().forEach(c -> {
-      if (c.getId() == id) {
-        c.setFirstName(customer.getFirstName());
-        c.setLastName(customer.getLastName());
-        c.setEmail(customer.getEmail());
-      }
-    });
+    customerList
+      .stream()
+      .forEach(c -> {
+        if (c.getId() == id) {
+          c.setFirstName(customer.getFirstName());
+          c.setLastName(customer.getLastName());
+          c.setEmail(customer.getEmail());
+        }
+      });
 
     return getCustomer(id);
   }
