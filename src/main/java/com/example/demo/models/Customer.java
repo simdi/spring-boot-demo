66a72e77552d5c808a77963ccf07812b973e3168
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2020-04-07 18:19:31 +0100

    Added H2 Database.

--- src/main/java/com/example/demo/models/Customer.java
+++ src/main/java/com/example/demo/models/Customer.java
@@ -1,8 +1,16 @@
 package com.example.demo.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@Entity
 public class Customer {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private int id;
   @JsonProperty("first_name")
   private String firstName;
