aabdfc1587108bff4a6f714e35cc26678be0b9a5
Chisimdi Damian Ezeanieto <simdi4iverson@yahoo.co.uk>
2020-04-08 08:11:43 +0100

    Handled errors in customer service.

---
+++ src/main/java/com/example/demo/api/ErrorHandler.java
@@ -0,0 +1,24 @@
+package com.example.demo.api;
+
+import com.example.demo.exception.ApplicationError;
+import com.example.demo.exception.CustomerNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class ErrorHandler extends ResponseEntityExceptionHandler {
+  @ExceptionHandler(CustomerNotFoundException.class)
+  public ResponseEntity<ApplicationError> handleCustomerNotFoundException(CustomerNotFoundException exception, WebRequest webRequest) {
+    ApplicationError error = new ApplicationError();
+    error.setCode(404);
+    error.setMessage(exception.getMessage());
+    return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+  }
+}
--- src/main/java/com/example/demo/services/CustomerService.java
+++ src/main/java/com/example/demo/services/CustomerService.java
@@ -1,12 +1,14 @@
 package com.example.demo.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.example.demo.dao.CustomerDAO;
+import com.example.demo.exception.CustomerNotFoundException;
 import com.example.demo.models.Customer;
 
 
@@ -24,7 +26,11 @@ public class CustomerService {
   }
 
   public Customer getCustomer(int id) {
-    return customerDAO.findById(id).get();
+    Optional<Customer> optionalCustomer = customerDAO.findById(id);
+    if (!optionalCustomer.isPresent()) {
+      throw new CustomerNotFoundException("Customer record not found.");
+    }
+    return optionalCustomer.get();
   }
 
   public Customer updateCustomer(int id, Customer customer) {
